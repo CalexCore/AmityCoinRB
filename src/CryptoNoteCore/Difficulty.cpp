9bdca461cedb506703ee1409f8bb6197a41d2d65
hooftly <hooftly@TINDEVM001.tf-dev.org>
2019-03-20 20:50:41 -0600

    changes

--- src/CryptoNoteCore/Difficulty.cpp
+++ src/CryptoNoteCore/Difficulty.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2018, The Calex Developers
 // 
 // Please see the included LICENSE file for more information.
 
@@ -80,18 +81,18 @@ uint64_t nextDifficultyV6(std::vector<uint64_t> timestamps, std::vector<uint64_t
       if ( i > N-3 ) { sum_3_ST += ST; }
     }
 
-    next_D = ((cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N+1) * 99) / (100 * 2 * L);
+    next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N+1) * 99) / (100 * 2 * L);
 
     prev_D = cumulativeDifficulties[N] - cumulativeDifficulties[N-1];
 
     next_D = std::max((prev_D * 67) / 100, std::min(next_D, (prev_D * 150) / 100));
 
-    if (sum_3_ST < (8 * T) / 10)
+    if (sum_3_ST < (9 * T) / 10)
     {  
         next_D = std::max(next_D, (prev_D * 108) / 100);
     }
 
-    return next_D;
+    return static_cast<uint64_t>(next_D);
 }
 
 // LWMA-2 difficulty algorithm 
@@ -108,21 +109,18 @@ uint64_t nextDifficultyV5(std::vector<uint64_t> timestamps, std::vector<uint64_t
        hashrate */
     if (timestamps.size() < static_cast<uint64_t>(N+1))
     {
-        return 10000;
+        return 5000;
     }
 
     for (int64_t i = 1; i <= N; i++)
     {  
-        ST = static_cast<int64_t>(timestamps[i]) - static_cast<int64_t>(timestamps[i-1]);
+      ST = static_cast<int64_t>(timestamps[i]) - static_cast<int64_t>(timestamps[i-1]);
 
-        ST = std::max(-4 * T, std::min(ST, 6 * T));
+      ST = std::max(-4 * T, std::min(ST, 6 * T));
 
-        L +=  ST * i; 
+      L +=  ST * i ;
 
-        if (i > N-3)
-        {
-            sum_3_ST += ST;
-        } 
+      if ( i > N-3 ) { sum_3_ST += ST; } 
     }
 
     next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N+1) * 99) / (100 * 2 * L);
@@ -150,7 +148,7 @@ uint64_t nextDifficultyV4(std::vector<uint64_t> timestamps, std::vector<uint64_t
 
     if (timestamps.size() <= static_cast<uint64_t>(N))
     {
-        return 1000;
+        return 5000;
     }
 
     for (int64_t i = 1; i <= N; i++)
@@ -191,7 +189,7 @@ uint64_t nextDifficultyV3(std::vector<uint64_t> timestamps, std::vector<uint64_t
 
     if (timestamps.size() <= static_cast<uint64_t>(N))
     {
-        return 1000;
+        return 5000;
     }
 
     for (int64_t i = 1; i <= N; i++)
@@ -219,3 +217,4 @@ uint64_t nextDifficultyV3(std::vector<uint64_t> timestamps, std::vector<uint64_t
 
     return static_cast<uint64_t>(next_D);
 }
+
